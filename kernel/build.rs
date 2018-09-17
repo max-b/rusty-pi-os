51fd25d3681f7dbfb7d85b14dab54d1e164c1172
Maxb <maxb.personal@gmail.com>
2018-09-17 11:29:53 -0400

    Adding fix to Makefile and build.rs for cargo xbuild target.

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,5 +1,5 @@
 pub fn main() {
-    if ::std::env::var("TARGET").unwrap() == "aarch64-none-elf" {
+    if ::std::env::var("TARGET").unwrap().contains("aarch64-none-elf") {
         println!("cargo:rustc-link-search=native=ext");
         println!("cargo:rustc-link-lib=static=sd");
         println!("cargo:rerun-if-changed=ext/libsd.a");
